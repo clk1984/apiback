25328677d90d3c6e6a368203dfd1166dae3a8b7a
fas84 <fernandoarozarena84@gmail.com>
2017-10-17 19:01:32 -0500

    refactoring

---
+++ README.md
@@ -0,0 +1,16 @@
+
+Photo like : A social networ to share images .
+
+This repostiory contains  a REST API made in Laravel that acts as the backend
+
+To run it locally:
+
+	-Composer install
+
+	-Copy env.example to .env and fill it with your database connection data.
+
+	-php artisan key:generate
+
+	-Run php artisan Passport install and paste the generated keys to .env
+
+	-Set storage/ and bootstrap/cache permissions to read and write
--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email','password'
+        'name', 'email'
     ];
 
     /**
@@ -28,8 +28,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-         'remember_token'
+         'remember_token','password'
     ];
+
     public function bordados()
     {
         return $this->hasMany('App\Bordado');
